c9ef8a819a192962154aa460a74f5ec335037cbe
Dan Wentworth <dan@atechmedia.com>
2019-05-31 17:02:40 +0100

    update event controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,8 @@
 class EventsController < ApplicationController
+  before_action :find_event
+
   def index
+    @events = current_user.events.by_upcoming
   end
 
   def new
@@ -16,9 +19,24 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(safe_event_params)
+      redirect_to events_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def safe_event_params
     params.require(:event).permit(:name, :website, :cfp_open_until, :date_of_event, :logo)
+  end
+
+  def find_event
+    @event = current_user.events.find(params[:id]) if params[:id]
   end
 end

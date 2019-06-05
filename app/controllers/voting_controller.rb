e0872e04aa4074f73786d71d7b909f336affa399
Dan Wentworth <dan@atechmedia.com>
2019-06-05 12:33:36 +0100

    rudimentary vote processing

--- app/controllers/voting_controller.rb
+++ app/controllers/voting_controller.rb
@@ -7,9 +7,22 @@ class VotingController < ApplicationController
   end
 
   def create
+    voter = Voter.create!(safe_voter_params)
+
+    params[:ratings].each do |submission_id, vote_weight|
+      submission = @event.submissions.find(submission_id)
+
+      voter.submission_votes.create!(submission: submission, weight: vote_weight)
+    end
+
+    render 'index'
   end
 
   private
+
+  def safe_voter_params
+    params.permit(:email_address)
+  end
 
   def find_event
     @event = Event.find_by!(id: params[:event_id], voting_token: params[:voting_token])

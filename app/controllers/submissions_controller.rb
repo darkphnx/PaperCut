db267d4f106e80e05c0900d3938475a676a9916f
Dan Wentworth <dan@atechmedia.com>
2019-06-05 13:49:22 +0100

    submissions now shown as table

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,8 +1,11 @@
 class SubmissionsController < ApplicationController
-  before_action :find_event
+  before_action :find_event, :find_submission
 
   def index
     @submissions = @event.submissions.by_popularity
+  end
+
+  def show
   end
 
   def upload
@@ -21,5 +24,9 @@ class SubmissionsController < ApplicationController
 
   def find_event
     @event = current_user.events.find(params[:event_id])
+  end
+
+  def find_submission
+    @submission = @event.submissions.find(params[:id]) if params[:id]
   end
 end

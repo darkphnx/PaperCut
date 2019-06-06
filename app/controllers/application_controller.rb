ddb54102825308d50d4739f967a179d528dcf13a
Dan Wentworth <dan@atechmedia.com>
2019-06-06 10:26:35 +0100

    catch authie errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :login_required
 
+  rescue_from Authie::Session::ValidityError, :with => :auth_session_error
+
   private
 
   def login_required
     redirect_to root_path unless logged_in?
+  end
+
+  def auth_session_error
+    redirect_to login_path, alert: "Your session was no longer valid. Please login to continue."
   end
 end
